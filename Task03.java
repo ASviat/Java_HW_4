df0477c597c609ba85beb0cc7f9c6e35b162779a
Sviatoslav <ecrprague.asi@gmail.com>
2023-03-15 00:11:06 +0300

    T3D But MB corrected

--- Task03.java
+++ Task03.java
@@ -25,59 +25,62 @@ public class Task03 {
 
     public static LinkedList<Integer> CalcIt(Logger logger, Scanner x, LinkedList<Integer> mList) {
 
-        // LinkedList<Integer> myList = new LinkedList<Integer>();
         System.out.println("Введите b для отмены последней операции, Введите q для выхода из программы");
 
         System.out.println("Введите число 1: ");
         String num1 = x.next();
-        if (num1 == "q") {
-            logger.info("Выход выполнен.");
-            return mList;
-        }
-        if (num1 == "b") {
-            mList.removeLast();
-            System.out.println(mList.getLast());
-            logger.info("Действие отменено.");
-            return CalcIt(logger, x, mList);
-        } 
-        else {
-            if (IsDigit(num1)) {
-                logger.info("Число 1 успешно введено.");
-                System.out.println("Введите действие: ");
-                char my_char = x.next().charAt(0);
-                logger.info("Действие успешно выбрано.");
-                System.out.println("Введите число 2: ");
-                var num2 = x.next();
-                if (IsDigit(num2)) {
-                    logger.info("Число  успешно введено.");
 
-                    System.out.println("Ваш результат: ");
+        if (IsDigit(num1)) {
+            logger.info("Число 1 успешно введено.");
+            System.out.println("Введите действие: ");
+            char my_char = x.next().charAt(0);
+            logger.info("Действие успешно выбрано.");
+            System.out.println("Введите число 2: ");
+            String num2 = x.next();
 
-                    switch (my_char) {
-                        case '+':
-                            mList.add(Integer.parseInt(num1) + Integer.parseInt(num2));
-                            break;
-                        case '-':
-                            mList.add(Integer.parseInt(num1) - Integer.parseInt(num2));
-                            break;
-                        case '*':
-                            mList.add(Integer.parseInt(num1) * Integer.parseInt(num2));
-                            break;
-                        case '%':
-                            mList.add(Integer.parseInt(num1) % Integer.parseInt(num2));
-                            break;
-                        case '/':
-                            mList.add(Integer.parseInt(num1) / Integer.parseInt(num2));
-                            break;
-                    }
-                    logger.info("Результат предоставлен.");
-                    System.out.println(mList.getLast());
-                    return CalcIt(logger, x, mList);
+            if (IsDigit(num2)) {
+                logger.info("Число  успешно введено.");
 
+                switch (my_char) {
+                    case '+':
+                        mList.add(Integer.parseInt(num1) + Integer.parseInt(num2));
+                        break;
+                    case '-':
+                        mList.add(Integer.parseInt(num1) - Integer.parseInt(num2));
+                        break;
+                    case '*':
+                        mList.add(Integer.parseInt(num1) * Integer.parseInt(num2));
+                        break;
+                    case '%':
+                        mList.add(Integer.parseInt(num1) % Integer.parseInt(num2));
+                        break;
+                    case '/':
+                        mList.add(Integer.parseInt(num1) / Integer.parseInt(num2));
+                        break;
                 }
+                logger.info("Результат предоставлен.");
+                
+                System.out.printf("%s %c %s = %s\n", num1, my_char, num2, mList.getLast());
+                return CalcIt(logger, x, mList);
+
+            } else {
+                System.out.println("Введите число!");
+                return CalcIt(logger, x, mList);
             }
+        } else {
+            if (num1.equals("q")) {
+                logger.info("Выход выполнен.");
+                return mList;
+            }
+            if (num1.equals("b")) {
+                mList.removeLast();
+                System.out.printf("Последний результат до отмены действия: %s\n", mList.getLast());
+                logger.info("Действие отменено.");
+                return CalcIt(logger, x, mList);
+            }
+            System.out.println("Моя твоя не понимать");
+            return CalcIt(logger, x, mList);
         }
-        return CalcIt(logger, x, mList);
     }
 
     private static boolean IsDigit(String s) throws NumberFormatException {
